b26ac0906de6622abf1bb1f85ec74932f2fbb076
steamxacademy <steamxacademy@gmail.com>
2026-06-28 17:57:29 +0530

    update

---
+++ js/science.js
@@ -0,0 +1,19 @@
+// Mouse Parallax
+
+const hero = document.querySelector(".hero");
+
+hero.addEventListener("mousemove",(e)=>{
+
+const x=e.clientX/window.innerWidth;
+const y=e.clientY/window.innerHeight;
+
+document.querySelectorAll(".floating").forEach((item,index)=>{
+
+const speed=(index+1)*15;
+
+item.style.transform=
+`translate(${x*speed}px,${y*speed}px)`;
+
+});
+
+});
